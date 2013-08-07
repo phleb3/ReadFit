a35e879d821f5c5ab7fbe4a81bb18869b2c8c3eb
phleb3 <mwiskochil@gmail.com>
2013-08-07 10:01:15 -0400

    First three files

---
+++ AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ReadFit.FileModel;
+
+namespace ReadFit
+{
+
+    class AboutViewModel : ObservableObject, IPageViewModel
+    {
+        public string Name
+        {
+            get { return "About"; }
+        }
+
+        MsgBoxService msgBoxobj;
+
+        public AboutViewModel()
+        {
+            msgBoxobj = new MsgBoxService();
+        }
+    }
+}
